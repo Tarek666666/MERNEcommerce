0d549ac340c77b467cf0c92361d0b55121d7cab3
Tarek <2b3zab666@gmail.com>
2023-02-19 18:01:55 +0100

    test

--- util/databse.js
+++ util/databse.js
@@ -1,27 +1,15 @@
 require('dotenv').config()
 
 
-/*
-const Sequelize  = require('sequelize');
-const sequelize = new Sequelize( 'shop' , 'root' , 'root', {
-    
-    dialect: 'mysql',
-    host: 'localhost'
 
-});
-
-
-module.exports = sequelize; 
-
-*/
-
+console.log(process.env.DATABASE)
 
 
 const Sequelize  = require('sequelize');
-const sequelize = new Sequelize( 'heroku_60f70c9307f1279' , 'b9a35914efcef8' , '19b25751', {
+const sequelize = new Sequelize(  process.env.DATABASE || 'shop' , process.env.USER || 'root' , process.env.PASSWORD ||  'root', {
     
     dialect: 'mysql',
-    host: 'eu-cdbr-west-03.cleardb.net'
+    host: process.env.HOST ||  'localhost'
 
 });
 
