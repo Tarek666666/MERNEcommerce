48ea5a86bd94441818820e2304df924985625363
Tarek <2b3zab666@gmail.com>
2023-02-19 02:50:10 +0100

    corrected db connection

--- util/databse.js
+++ util/databse.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 
+console.log(process.env.CLEARDB_DATABASE_URL)
 
 
 const Sequelize  = require('sequelize');
-const sequelize = new Sequelize( process.env.DATABASE , process.env.USER , process.env.PASSWORD, {
+const sequelize = new Sequelize( 'heroku_60f70c9307f1279' , 'b9a35914efcef8' , '19b25751', {
     
     dialect: 'mysql',
-    host:process.env.HOST
+    host: 'eu-cdbr-west-03.cleardb.net'
 
 });
 
