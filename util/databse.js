437b610a770c575c3a966e91dd7ab887c22785c6
Tarek <2b3zab666@gmail.com>
2023-02-19 03:09:08 +0100

    t

--- util/databse.js
+++ util/databse.js
@@ -4,7 +4,7 @@ console.log(process.env.CLEARDB_DATABASE_URL)
 
 
 const Sequelize  = require('sequelize');
-const sequelize = new Sequelize( 'mysql://b9a35914efcef8:19b25751@eu-cdbr-west-03.cleardb.net/heroku_60f70c9307f1279?reconnect=true' , 'b9a35914efcef8' , '19b25751', {
+const sequelize = new Sequelize( 'heroku_60f70c9307f1279' , 'b9a35914efcef8' , '19b25751', {
     
     dialect: 'mysql',
     host: 'eu-cdbr-west-03.cleardb.net'
